199157d5744cc6eb5b14357aad3694b3c934568b
Jerome Shapiro <jerome@dimebox.com>
2018-07-13 15:25:42 +0200

    Created random uint type def

--- src/crypto/rand.c
+++ src/crypto/rand.c
@@ -2,21 +2,22 @@
 #include <openssl/rand.h>
 #include "rand.h"
 
+typedef union {
+    unsigned int uint;
+    unsigned char c[sizeof(unsigned int)];
+} _rand_uint;
+
 unsigned int rand_uint(unsigned int min, unsigned int max) {
     unsigned int offset = max - min;
+    _rand_uint byte;
 
-    union {
-        unsigned int i;
-        unsigned char c[sizeof(unsigned int)];
-    } u;
-
-    do {
-        if (!RAND_bytes(u.c, sizeof(u.c))) {
+    while (byte.uint < (-offset % offset)) {
+        if (!RAND_bytes(byte.c, sizeof(byte.c))) {
             fprintf(stderr, "Can't get random bytes!\n");
             exit(1);
         }
-    } while (u.i < (-offset % offset));
+    }
 
-    return (u.i % offset) + min;
+    return (byte.uint % offset) + min;
 }
 
